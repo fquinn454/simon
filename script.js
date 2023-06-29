615517e03dc3d55432dc1ab40b9f38ceaf32aed0
Fiona <57137659+fquinn454@users.noreply.github.com>
2023-06-29 15:21:06 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function playGreenSound(){
+    var snd = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3')//wav is also supported
+    snd.play()//plays the sound
+}
+
+function playRedSound(){
+    var snd = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3')//wav is also supported
+    snd.play()//plays the sound
+}
+
+function playBlueSound(){
+    var snd = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3')//wav is also supported
+    snd.play()//plays the sound
+}
+
+function playYellowSound(){
+    var snd = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')//wav is also supported
+    snd.play()//plays the sound
+}
+
